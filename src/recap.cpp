8401cf31afa018502de28b730aac18f5bd2e7f77
Paul Ullrich <paullrich@ucdavis.edu>
2017-03-08 16:52:46 -0800

    - Added simple parser to recap.cpp

--- src/recap.cpp
+++ src/recap.cpp
@@ -21,7 +21,10 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
+#include <vector>
+#include <stack>
 
 #if defined(HYPERION_MPIOMP)
 #include <mpi.h>
@@ -31,7 +34,7 @@
 
 int main(int argc, char** argv) {
 
-#if defined(TEMPEST_MPIOMP)
+#if defined(HYPERION_MPIOMP)
 	// Initialize MPI
 	MPI_Init(&argc, &argv);
 #endif
@@ -55,11 +58,247 @@ try {
 		return (-1);
 	}
 
+	// Load command-line variables
+
+
+	// Begin parsing
+	int iLine = 0;
+
+	for (std::string strLine; std::getline(ifScript, strLine); ) {
+
+		// Increment line number 
+		iLine++;
+
+		// Parser state
+		enum ParserState {
+			ParserState_WS,
+			ParserState_Token,
+			ParserState_String,
+			ParserState_Op
+		};
+
+		// Parsed command line
+		std::vector<std::string> vecCommandLine;
+		std::vector<ParserState> vecCommandLineType;
+
+		ParserState parse_state = ParserState_WS;
+
+		std::stack<char> stParentheses;
+
+		// Begin parsing
+		bool fError = false;
+		int iPos = 0;
+		for (int i = 0; i < strLine.length();) {
+
+			// Whitespace
+			if (parse_state == ParserState_WS) {
+				if ((strLine[i] == ' ') || (strLine[i] == '\t')) {
+					i++;
+					iPos = i;
+					continue;
+
+				} else if (strLine[i] == '#') {
+					break;
+
+				} else if (((strLine[i] >= 'A') && (strLine[i] <= 'Z')) ||
+				    ((strLine[i] >= 'a') && (strLine[i] <= 'z')) ||
+					(strLine[i] == '_')
+				) {
+					parse_state = ParserState_Token;
+					continue;
+
+				} else if (
+					(strLine[i] == '=') ||
+					(strLine[i] == '(') ||
+					(strLine[i] == ')') ||
+					(strLine[i] == '[') ||
+					(strLine[i] == ']') ||
+					(strLine[i] == ',') ||
+					(strLine[i] == '\"') ||
+					(strLine[i] == '.')
+				) {
+					if (vecCommandLine.size() == 0) {
+						Announce("ERROR: Unexpected character on line %i (%i)", iLine, i);
+						fError = true;
+						break;
+					}
+					parse_state = ParserState_Op;
+					continue;
+
+				} else {
+					Announce("ERROR: Unexpected character on line %i (%i)", iLine, i);
+					fError = true;
+					break;
+				}
+
+			// Token
+			} else if (parse_state == ParserState_Token) {
+				if (((strLine[i] >= 'A') && (strLine[i] <= 'Z')) ||
+				    ((strLine[i] >= 'a') && (strLine[i] <= 'z')) ||
+					(strLine[i] == '_')
+				) {
+					i++;
+					continue;
+
+				} else {
+					if (iPos != i) {
+						vecCommandLine.push_back(
+							strLine.substr(iPos, i-iPos));
+						vecCommandLineType.push_back(
+							ParserState_Token);
+						iPos = i;
+					}
+
+					if (
+						(strLine[i] == '=') ||
+						(strLine[i] == '(') ||
+						(strLine[i] == ')') ||
+						(strLine[i] == '[') ||
+						(strLine[i] == ']') ||
+						(strLine[i] == ',') ||
+						(strLine[i] == '\"') ||
+						(strLine[i] == '.')
+					) {
+						parse_state = ParserState_Op;
+						continue;
+
+					} else if (strLine[i] == ' ') {
+						parse_state = ParserState_WS;
+						continue;
+					}
+				}
+
+			// String
+			} else if (parse_state == ParserState_String) {
+				if (strLine[i] == '\"') {
+					vecCommandLine.push_back(
+						strLine.substr(iPos, i-iPos));
+					vecCommandLineType.push_back(
+						ParserState_String);
+					parse_state = ParserState_Op;
+					continue;
+				}
+				i++;
+
+			// Op
+			} else if (parse_state == ParserState_Op) {
+				if (strLine[i] == ' ') {
+					parse_state = ParserState_WS;
+					continue;
+				}
+
+				if (strLine[i] == '(') {
+					stParentheses.push('(');
+					vecCommandLine.push_back("(");
+					vecCommandLineType.push_back(ParserState_Op);
+					parse_state = ParserState_Token;
+
+				} else if (strLine[i] == '[') {
+					stParentheses.push('[');
+					vecCommandLine.push_back("[");
+					vecCommandLineType.push_back(ParserState_Op);
+					parse_state = ParserState_Token;
+
+				} else if (strLine[i] == '\"') {
+					if ((stParentheses.size() != 0) &&
+					    (stParentheses.top() == '\"')
+					) {
+						stParentheses.pop();
+
+					} else {
+						stParentheses.push('\"');
+						parse_state = ParserState_String;
+					}
+
+				} else if (strLine[i] == ')') {
+					if (stParentheses.size() == 0) {
+						Announce("ERROR: Unbalanced parenthesis on line %i (%i)", iLine, i);
+						fError = true;
+						break;
+					}
+					if (stParentheses.top() != '(') {
+						Announce("ERROR: Unbalanced parenthesis on line %i (%i)", iLine, i);
+						fError = true;
+						break;
+					}
+					stParentheses.pop();
+					vecCommandLine.push_back(")");
+					vecCommandLineType.push_back(ParserState_Op);
+
+				} else if (strLine[i] == ']') {
+					if (stParentheses.size() == 0) {
+						Announce("ERROR: Unbalanced bracket on line %i (%i)", iLine, i);
+						fError = true;
+						break;
+					}
+					if (stParentheses.top() != '[') {
+						Announce("ERROR: Unbalanced bracket on line %i (%i)", iLine, i);
+						fError = true;
+						break;
+					}
+					stParentheses.pop();
+					vecCommandLine.push_back("]");
+					vecCommandLineType.push_back(ParserState_Op);
+
+				} else if (strLine[i] == '=') {
+					vecCommandLine.push_back("=");
+					vecCommandLineType.push_back(ParserState_Op);
+					parse_state = ParserState_Token;
+
+				} else if (strLine[i] == ',') {
+					vecCommandLine.push_back(",");
+					vecCommandLineType.push_back(ParserState_Op);
+					parse_state = ParserState_Token;
+
+				} else if (strLine[i] == '.') {
+					vecCommandLine.push_back(".");
+					vecCommandLineType.push_back(ParserState_Op);
+					parse_state = ParserState_Token;
+
+				} else {
+					Announce("ERROR: Unexpected character on line %i (%i)", iLine, i);
+					fError = true;
+					break;
+				}
+
+				i++;
+				iPos = i;
+			}
+		}
+		if (vecCommandLine.size() != vecCommandLineType.size()) {
+			_EXCEPTIONT("CommandLine/CommandLineType mismatch");
+		}
+
+		if (vecCommandLine.size() == 0) {
+			continue;
+		}
+
+		if (parse_state == ParserState_String) {
+			Announce("ERROR: Unbalanced quotation marks on line %i", iLine);
+			fError = true;
+		}
+
+		if (stParentheses.size() != 0) {
+			Announce("ERROR: Unbalanced parenthesis or bracket on line %i", iLine);
+			std::cout << stParentheses.top() << std::endl;
+			fError = true;
+		}
+
+		if (fError) {
+			return (-1);
+		}
+
+		for (int i = 0; i < vecCommandLine.size(); i++) {
+			printf("%s:", vecCommandLine[i].c_str());
+		}
+		printf("\n");
+	}
+
 } catch(Exception & e) {
 	Announce(e.ToString().c_str());
 }
 
-#if defined(TEMPEST_MPIOMP)
+#if defined(HYPERION_MPIOMP)
 	// Deinitialize MPI
 	MPI_Finalize();
 #endif
